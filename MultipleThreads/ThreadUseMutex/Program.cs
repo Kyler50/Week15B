bf9a024cab03ebf9171ec3f761c9ba7686b531fa
Vas Richard <kyler50vasrich@gmail.com>
2016-05-04 15:24:07 +0200

    Serialization isn't finish yet

---
+++ MultipleThreads/ThreadUseMutex/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadUseMutex
+{
+    using System.Threading;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex oneMutex = null;
+            const string MutexName = "RUNMEONLYONCE";
+
+            try 
+            {
+                oneMutex = Mutex.OpenExisting(MutexName);
+            }
+            catch (WaitHandleCannotBeOpenedException)
+            {
+            }
+
+            if (oneMutex == null)
+            {
+                oneMutex = new Mutex(true, MutexName);
+            }
+            else
+            {
+                oneMutex.Close();
+                return;
+            }
+
+            Console.WriteLine("Our Application");
+            Console.Read();
+        }
+    }
+
+}
---
+++ Searcher/Searcher/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Searcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            resultList.Items.Clear();
+            foreach (var line in textBox.Lines)
+            {
+                MatchCollection matchCollection = Regex.Matches(line, patternBox.Text);
+
+                if (matchCollection.Count > 0)
+                {
+                    foreach (System.Text.RegularExpressions.Match match in matchCollection)
+                    {
+                        resultList.Items.Add(match);
+                    }
+                }
+            }
+        }
+    }
+}
